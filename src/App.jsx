1908db36f748e36578531496f50cdcdb566b56be
let-sampaio <leticia.sampaio.souza08@aluno.ifce.edu.br>
2024-04-11 19:07:56 -0300

    ✨uma telinha

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import LoginForm from './Components/Assets/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <>
+      <div>
+        <LoginForm/>
+      </div>
+       
+    </>
+  );
+}
+
+export default App
